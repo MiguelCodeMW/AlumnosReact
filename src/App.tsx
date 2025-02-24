a75d57c4c055da623bb4863f0bbf115285e5a6e9
Miguel Ángel García Sánchez <miguelangel@MacBook-Air-de-Miguel.local>
2025-02-24 16:59:56 +0100

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import Lista from "./components/Lista";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Formulario from "./components/Formulario";
 
 function App() {
   const handleEditar = (id: number) => console.log(`Editar ${id}`);
   const handleEliminar = (id: number) => console.log(`Eliminar ${id}`);
 
-  return (
+  /* return (
     <Lista irAEditarAlumno={handleEditar} eliminarAlumno={handleEliminar} />
+  ); */
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/alumno" />} />
+        <Route
+          path="/alumno"
+          element={
+            <Lista
+              irAEditarAlumno={handleEditar}
+              eliminarAlumno={handleEliminar}
+            />
+          }
+        />
+        <Route path="/alumno/create" element={<Formulario />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
--- src/components/Lista.tsx
+++ src/components/Lista.tsx
@@ -2,6 +2,7 @@ import Button from "./Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Alumno } from "../utils/Alumno";
+import { useNavigate } from "react-router-dom";
 
 type listProps = {
   irAEditarAlumno: (id: number) => void;
@@ -10,6 +11,7 @@ type listProps = {
 
 function Lista({ irAEditarAlumno, eliminarAlumno }: listProps) {
   const [alumnos, setAlumnos] = useState<Alumno[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Obtener los posts desde Laravel
@@ -22,7 +24,12 @@ function Lista({ irAEditarAlumno, eliminarAlumno }: listProps) {
         console.error("Error fetching posts:", error);
       });
   }, []);
-  if (alumnos.length > 0) {
+
+  const handleCreateAlumno = () => {
+    navigate("/alumno/create");
+  };
+
+  /* if (alumnos.length > 0) {
     return (
       <ul>
         {alumnos.map((alumno) => (
@@ -45,6 +52,38 @@ function Lista({ irAEditarAlumno, eliminarAlumno }: listProps) {
     );
   } else {
     return <p>No hay alumnos inscritos</p>;
+  } */
+  if (alumnos.length > 0) {
+    return (
+      <>
+        <Button text="Crear alumno" onClick={handleCreateAlumno} />
+        <ul>
+          {alumnos.map((alumno) => (
+            <li key={alumno.id}>
+              {alumno.nombre}
+              <br />
+              <Button
+                text="Editar"
+                id={alumno.id}
+                onClick={() => irAEditarAlumno(alumno.id)}
+              />
+              <Button
+                text="Eliminar"
+                id={alumno.id}
+                onClick={() => eliminarAlumno(alumno.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Button text="Crear alumno" onClick={handleCreateAlumno} />
+        <p>No hay alumnos inscritos</p>
+      </>
+    );
   }
 }
 
