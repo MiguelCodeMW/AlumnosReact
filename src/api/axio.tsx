7b32fe10912107beeed53ebfdc1149e02ee3e0c6
Miguel Ángel García Sánchez <example@gmail.com>
2025-03-06 00:51:58 +0100

    url

--- src/api/axio.tsx
+++ src/api/axio.tsx
@@ -1,11 +1,11 @@
 // src/api/axios.js
 import axios from "axios";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-  baseURL: API_BASE_URL,
-  // baseURL: "http:desplieguedemiaplicacion.duckdns.org",
+  //baseURL: API_BASE_URL,
+  baseURL: "http:desplieguedemiaplicacion.duckdns.org",
   headers: {
     "Content-Type": "application/json",
   },
